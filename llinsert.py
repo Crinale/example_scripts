0c15e8b243068fe2fb951695a51bc2bfed7a497d
Crinale <devonchu@hotmail.com>
2018-11-02 21:26:57 -0700

    llinsert has my logic down just wont compile

--- llinsert.py
+++ llinsert.py
@@ -36,11 +36,19 @@ def print_singly_linked_list(node, sep, fptr):
 #
 #
 def insertNodeAtTail(head, data):
-
-
-
-    
-
+    #need something that will allow it to reference what llist is.
+    #base case if there is nothing at all in the linked list
+    if head == None:
+        return SinglyLinkedListNode(data)
+    else:
+    #If there is something in the link list and the next is none add a node
+        if head.next == None:
+            head.next = SinglyLinkedListNode(data)
+        else:
+            #if the next node has something keep going until you see none in the next
+            insertNodeAtTail(head.next,data)
+        return head
+        
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
