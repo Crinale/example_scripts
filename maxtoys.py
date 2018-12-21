cc36059d3cb731b08cc8aa5b7fbc92edd70dfeee
Crinale <devonchu@hotmail.com>
2018-12-20 22:20:36 -0800

    sorting then using for loop

---
+++ maxtoys.py
@@ -0,0 +1,35 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the maximumToys function below.
+def maximumToys(prices, k):
+    prices.sort()
+    toys = 0
+    for i in prices:
+        if i <= k:
+            k -= i
+            toys +=1 
+    return toys
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = raw_input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    prices = map(int, raw_input().rstrip().split())
+
+    result = maximumToys(prices, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
